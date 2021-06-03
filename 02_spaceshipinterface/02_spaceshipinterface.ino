2ee4c60b4bd6a41ecbd0c3388e779225dd2e313e
devways <thomas.loyan.dev@gmail.com>
2021-06-03 17:51:28 +0200

    change how the switch works

--- 02_spaceshipinterface/02_spaceshipinterface.ino
+++ 02_spaceshipinterface/02_spaceshipinterface.ino
@@ -19,45 +19,48 @@
   This example code is part of the public domain.
 */
 
-// Create a global variable to hold the state of the switch. This variable is
-// persistent throughout the program. Whenever you refer to switchState, you’re
-// talking about the number it holds
-int switchstate = 0;
+int switchState = 0;
+int previousSwitchState = 0;
+bool mode = false;
+
+bool buttonIsPressed() {
+  if (switchState == true && previousSwitchState == false) {
+    return true;
+  }
+  return false;
+}
+
+void changeMode() {
+  mode = !mode;
+}
 
 void setup() {
-  // declare the LED pins as outputs
+  pinMode(2, INPUT);
+
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
-
-  // declare the switch pin as an input
-  pinMode(2, INPUT);
 }
 
 void loop() {
+  switchState = digitalRead(2);
 
-  // read the value of the switch
-  // digitalRead() checks to see if there is voltage on the pin or not
-  switchstate = digitalRead(2);
+  if (buttonIsPressed()) changeMode();
 
-  // if the button is not pressed turn on the green LED and off the red LEDs
-  if (switchstate == LOW) {
+  if (!mode) {
     digitalWrite(3, HIGH); // turn the green LED on pin 3 on
     digitalWrite(4, LOW);  // turn the red LED on pin 4 off
     digitalWrite(5, LOW);  // turn the red LED on pin 5 off
-  }
-  // this else is part of the above if() statement.
-  // if the switch is not LOW (the button is pressed) turn off the green LED and
-  // blink alternatively the red LEDs
-  else {
+  } else {
     digitalWrite(3, LOW);  // turn the green LED on pin 3 off
     digitalWrite(4, LOW);  // turn the red LED on pin 4 off
     digitalWrite(5, HIGH); // turn the red LED on pin 5 on
-    // wait for a quarter second before changing the light
     delay(250);
     digitalWrite(4, HIGH); // turn the red LED on pin 4 on
     digitalWrite(5, LOW);  // turn the red LED on pin 5 off
-    // wait for a quarter second before changing the light
     delay(250);
   }
+
+  if (previousSwitchState == switchState) return;
+  previousSwitchState = switchState;
 }
